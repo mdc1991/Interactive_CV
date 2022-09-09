7ecae35ce83d3bef4f3af101f26e5ac13c7959b8
MarkCooper <markcooper91@hotmail.co.uk>
2022-09-09 15:40:36 +0100

    update __name__ to app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import dash
 import dash_bootstrap_components as dbc
 
 #Instantiates the Dash app and identify the server
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
